bdd13761fa70c53092f75726122bcec0649a355a
tranquoctuan2001 <76949884+tranquoctuan2001@users.noreply.github.com>
2022-03-07 23:09:56 +0700

    User interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
+
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {
+  }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (respone: Employee[]) => {
+        this.employees = respone;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+
+    )
+  }
 }
