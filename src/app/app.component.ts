2b40ace991e9833a4591996b4e21cabf71cff097
tranquoctuan2001 <76949884+tranquoctuan2001@users.noreply.github.com>
2022-03-29 14:47:05 +0700

    FE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { EmployeeService } from './employee.service';
 import { Employee } from './employee';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
 
   public employees: Employee[];
+  public editEmployee: Employee;
 
-  constructor(private employeeService: EmployeeService) {
-  }
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
     this.getEmployees();
@@ -24,11 +25,77 @@ export class AppComponent implements OnInit {
     this.employeeService.getEmployees().subscribe(
       (respone: Employee[]) => {
         this.employees = respone;
+        this.editEmployee = this.employees[0];
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
+    );
+  }
 
-    )
+  public onOpenModal(employee: Employee, modal: string): void {
+    const button = document.createElement('button');
+    const container = document.getElementById('main-container');
+
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (modal === 'add') {
+      button.setAttribute('data-target', '#addEmployee');
+    }
+    if (modal === 'edit') {
+      this.editEmployee = employee;
+
+      button.setAttribute('data-target', '#editEmployee');
+    }
+    if (modal === 'delete') {
+      this.editEmployee = employee;
+      console.log(this.editEmployee);
+      button.setAttribute('data-target', '#deleteEmployee');
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    // document.getElementById("add-employee-form").click();
+    this.employeeService.addEmployees(addForm.value).subscribe(
+      (respone: Employee) => {
+        console.log(respone);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error);
+      }
+    );
+  }
+
+  public onUpdateEmployee(employee: Employee): void {
+    const hiddenEdit = document.getElementById('hidden-edit');
+    // document.getElementById("add-employee-form").click();
+    this.employeeService.updateEmployees(employee).subscribe(
+      (respone: Employee) => {
+        console.log(respone);
+        this.getEmployees();
+        hiddenEdit.click();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error);
+      }
+    );
+  }
+
+  public onDeleteEmployee(employee: Employee): void {
+    const hiddenBtn = document.getElementById('btn-close-delete');
+    this.employeeService.deleteEmployees(employee.id).subscribe(
+      (response) => {
+        this.getEmployees();
+        alert('Delete sucessfully employee id' + employee.id);
+        hiddenBtn.click();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error);
+      }
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, FormsModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
